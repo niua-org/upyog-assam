d32ed0db2387ff1d2c8fc2dfbc9aa37a8297ff7c
Mridul Baishya <mridulbaishya28@gmail.com>
2024-10-18 16:24:36 +0530

    Added workflow of bills

---
+++ finance/egov/egov-egf/src/main/resources/db/migration/sample/V20210331145629__egf_workflow_data.sql
@@ -0,0 +1,6 @@
+-- changing of designation will effect this.
+-- change designations accrodingly here also
+
+update eg_wf_matrix set currentdesignation='Accounts Officer,Executive Officer', nextdesignation='Commissioner,Accounts Officer' where id=32;
+update eg_wf_matrix set currentdesignation='Accounts Officer,Executive Officer', nextdesignation='Commissioner,Accounts Officer' where id=26;
+update eg_wf_matrix set currentdesignation='Accounts Officer,Executive Officer', nextdesignation='Commissioner,Accounts Officer' where id=29;
